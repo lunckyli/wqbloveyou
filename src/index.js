aad934d8597116974441c2ffac10b4572f9bdd79
lunckyli <452966955@qq.com>
2018-09-06 11:22:20 +0800

    11
    11

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import GalleryByReactApp from './components/GalleryByReactApp';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<GalleryByReactApp />, document.getElementById('root'));
-registerServiceWorker();
